7671d551b2f442ca8dd10f03f8a86e0c4a051dd4
leum-i <77368897+leum-i@users.noreply.github.com>
2021-01-24 14:14:16 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const weather = document.querySelector(".weather");
 const weatherIcon = document.querySelector(".weatherIcon");
 const weatherPlace = document.querySelector(".location")
 
-const API_KEY = "4618b02dd5ea048ccac439c539e279ea";
+const API_KEY = config.MY_API_KEY;
 const COORDS = 'coords';
 
 function getWeather(lat, lon){
